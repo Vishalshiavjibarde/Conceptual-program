c6395b0e2d9aa9af793735e202dbd9ef9ea6ae5b
Barde Vishal Shivaji <95370589+Vishalshiavjibarde@users.noreply.github.com>
2022-12-29 21:10:03 +0530

    Add files via upload

---
+++ java programs/Combination4.java
@@ -0,0 +1,17 @@
+// Case 4
+//Not Allowed
+class Demo 
+{
+    int i;
+    void fun()
+    {}
+}
+class Hello
+{
+    int i;
+    void fun()
+    {}
+}
+class Marvellous extends Demo, Hello    // Error
+{
+}
